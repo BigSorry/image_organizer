05f9f71614b7a1567fbc8804d0ce9a3f65530b74
BigSorry <lexmeulenkamp@hotmail.nl>
2025-12-25 14:07:34 +0100

    add reading media

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import cv2
+import glob
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def read_frames(path):
+    cap = cv2.VideoCapture(path)
+    if not cap.isOpened():
+        return []
 
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    cap.release()
+    return frames
+
+def read_media(folder_path, image_only=True):
+    IMAGE_EXTS = (".jpg", ".jpeg", ".png", ".bmp", ".tiff")
+    VIDEO_EXTS = (".mp4", ".avi", ".mov", ".mkv")
+    images = []
+    videos = []
+    for name in folder_path:
+        ext = name.lower()
+        if ext.endswith(IMAGE_EXTS):
+            img = cv2.imread(name)
+            if img is None:
+                print(f"Failed to read {name}")
+                continue
+            images.append(img)
+        elif ext.endswith(VIDEO_EXTS) and image_only is False:
+            frames = read_frames(name)
+            if frames:
+                videos.append(frames)
+
+    return images, videos
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    input_path = "F:/DCIM/DJI_001/*.*"
+    image_paths = sorted(glob.glob(input_path))
+    image_paths = image_paths[:10]
+    images, videos = read_media(image_paths)
+    print(images, videos)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
